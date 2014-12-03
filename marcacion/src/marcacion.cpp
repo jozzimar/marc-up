c1014c40e7b101ff8cd5dacb1642de5890049ab9
jose luis <joseluis8906@openmailbox.org>
2014-12-02 20:28:39 -0500

    listo la marcacion

--- marcacion/src/marcacion.cpp
+++ marcacion/src/marcacion.cpp
@@ -80,8 +80,12 @@ bool Marcacion::key_enter (GdkEventKey* event)
 		this->label_id_clerk.set_text("Numero de Identificación");
 		this->entry_id_clerk.set_text("");
  		this->entry_id_clerk.set_visibility (true);
- 		sleep(3);
- 		this->label_marker_register.set_text("MARCACION REGISTRADA...");
+ 		
+ 		this->label_marker_register.override_color ();
+ 		this->label_marker_register.set_text("Marcación registrada.");
+ 		
+ 		sigc::slot<bool> my_slot = sigc::bind (sigc::mem_fun(*this, &Marcacion::reset_label_marcacion), 0);
+ 		sigc::connection conn = Glib::signal_timeout ().connect (my_slot, 3000);
  		
  		this->press=0;
  		 		
@@ -90,3 +94,9 @@ bool Marcacion::key_enter (GdkEventKey* event)
 	return false;
 }
 
+bool Marcacion::reset_label_marcacion (int valor)
+{	
+	this->label_marker_register.set_text ("");
+	
+	return false;
+}
